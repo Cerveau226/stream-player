1e8857a57513ef8ba931600d9500e6adee5e1d9d
Cerveau226 <152230523+Cerveau226@users.noreply.github.com>
2026-03-09 02:36:34 +0000

    Create App.js with the main application code

---
+++ App.js
@@ -0,0 +1,16 @@
+// Main application code
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* Add your screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
